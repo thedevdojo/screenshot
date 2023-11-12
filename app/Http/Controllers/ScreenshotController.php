08f4bfd25ccb3577071b0d598b1c3c7a6fd0bd6f
Tony Lea <tnylea@gmail.com>
2023-11-12 12:09:49 -0500

    Adding default dimensions as 16:9 aspect-video

--- app/Http/Controllers/ScreenshotController.php
+++ app/Http/Controllers/ScreenshotController.php
@@ -13,6 +13,7 @@ class ScreenshotController extends Controller
 
         $screenshot = Browsershot::url($request->url)
             ->setChromePath('/snap/bin/chromium')
+            ->windowSize(1920, 1080)
             ->newHeadless()
             ->noSandbox()
             ->timeout(120)
@@ -25,16 +26,21 @@ class ScreenshotController extends Controller
 
     public function snapFromHtml(Request $request)
     {
+        dd('hit');
         $request->validate(['html' => 'required|string']);
+        
         $html = '<html><head><script src="https://cdn.tailwindcss.com"></script></head><body>' . $request->html . '</body></html>';
 
         $screenshot = Browsershot::html($html)
             ->setChromePath('/snap/bin/chromium')
+            ->windowSize(1920, 1080)
             ->newHeadless()
             ->noSandbox()
             ->timeout(120)
             ->setContentUrl('https://www.example.com')
             ->screenshot();
+
+        
 
         return response($screenshot, 200, [
             'Content-Type' => 'image/png',
